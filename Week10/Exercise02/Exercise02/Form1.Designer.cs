a8ecaea158c1cd0595c83c02b4881397541a7945
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-05-28 20:57:58 +0700

    added Week10

---
+++ Week10/Exercise02/Exercise02/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Exercise02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercise02 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query01ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query02ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query03ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query04ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query05ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query06ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query07ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gridView = new System.Windows.Forms.DataGridView();
+            this.exercise03ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.query01ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query02ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query03ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query04ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query05ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query06ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.query07ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.exercise02,
+            this.exercise03ToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadDatabaseToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exercise02
+            // 
+            this.exercise02.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.query01ToolStripMenuItem,
+            this.query02ToolStripMenuItem,
+            this.query03ToolStripMenuItem,
+            this.query04ToolStripMenuItem,
+            this.query05ToolStripMenuItem,
+            this.query06ToolStripMenuItem,
+            this.query07ToolStripMenuItem});
+            this.exercise02.Name = "exercise02";
+            this.exercise02.Size = new System.Drawing.Size(76, 20);
+            this.exercise02.Text = "Exercise 02";
+            // 
+            // query01ToolStripMenuItem
+            // 
+            this.query01ToolStripMenuItem.Name = "query01ToolStripMenuItem";
+            this.query01ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query01ToolStripMenuItem.Text = "Query 01";
+            this.query01ToolStripMenuItem.Click += new System.EventHandler(this.query01ToolStripMenuItem_Click);
+            // 
+            // query02ToolStripMenuItem
+            // 
+            this.query02ToolStripMenuItem.Name = "query02ToolStripMenuItem";
+            this.query02ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query02ToolStripMenuItem.Text = "Query 02";
+            this.query02ToolStripMenuItem.Click += new System.EventHandler(this.query02ToolStripMenuItem_Click);
+            // 
+            // query03ToolStripMenuItem
+            // 
+            this.query03ToolStripMenuItem.Name = "query03ToolStripMenuItem";
+            this.query03ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query03ToolStripMenuItem.Text = "Query 03";
+            this.query03ToolStripMenuItem.Click += new System.EventHandler(this.query03ToolStripMenuItem_Click);
+            // 
+            // query04ToolStripMenuItem
+            // 
+            this.query04ToolStripMenuItem.Name = "query04ToolStripMenuItem";
+            this.query04ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query04ToolStripMenuItem.Text = "Query 04";
+            this.query04ToolStripMenuItem.Click += new System.EventHandler(this.query04ToolStripMenuItem_Click);
+            // 
+            // query05ToolStripMenuItem
+            // 
+            this.query05ToolStripMenuItem.Name = "query05ToolStripMenuItem";
+            this.query05ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query05ToolStripMenuItem.Text = "Query 05";
+            this.query05ToolStripMenuItem.Click += new System.EventHandler(this.query05ToolStripMenuItem_Click);
+            // 
+            // query06ToolStripMenuItem
+            // 
+            this.query06ToolStripMenuItem.Name = "query06ToolStripMenuItem";
+            this.query06ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query06ToolStripMenuItem.Text = "Query 06";
+            this.query06ToolStripMenuItem.Click += new System.EventHandler(this.query06ToolStripMenuItem_Click);
+            // 
+            // query07ToolStripMenuItem
+            // 
+            this.query07ToolStripMenuItem.Name = "query07ToolStripMenuItem";
+            this.query07ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.query07ToolStripMenuItem.Text = "Query 07";
+            this.query07ToolStripMenuItem.Click += new System.EventHandler(this.query07ToolStripMenuItem_Click);
+            // 
+            // loadDatabaseToolStripMenuItem
+            // 
+            this.loadDatabaseToolStripMenuItem.Name = "loadDatabaseToolStripMenuItem";
+            this.loadDatabaseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadDatabaseToolStripMenuItem.Text = "Exit";
+            this.loadDatabaseToolStripMenuItem.Click += new System.EventHandler(this.loadDatabaseToolStripMenuItem_Click);
+            // 
+            // gridView
+            // 
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridView.Location = new System.Drawing.Point(0, 24);
+            this.gridView.Name = "gridView";
+            this.gridView.Size = new System.Drawing.Size(800, 426);
+            this.gridView.TabIndex = 2;
+            // 
+            // exercise03ToolStripMenuItem
+            // 
+            this.exercise03ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.query01ToolStripMenuItem1,
+            this.query02ToolStripMenuItem1,
+            this.query03ToolStripMenuItem1,
+            this.query04ToolStripMenuItem1,
+            this.query05ToolStripMenuItem1,
+            this.query06ToolStripMenuItem1,
+            this.query07ToolStripMenuItem1});
+            this.exercise03ToolStripMenuItem.Name = "exercise03ToolStripMenuItem";
+            this.exercise03ToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.exercise03ToolStripMenuItem.Text = "Exercise 03";
+            // 
+            // query01ToolStripMenuItem1
+            // 
+            this.query01ToolStripMenuItem1.Name = "query01ToolStripMenuItem1";
+            this.query01ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query01ToolStripMenuItem1.Text = "Query 01";
+            this.query01ToolStripMenuItem1.Click += new System.EventHandler(this.query01ToolStripMenuItem1_Click);
+            // 
+            // query02ToolStripMenuItem1
+            // 
+            this.query02ToolStripMenuItem1.Name = "query02ToolStripMenuItem1";
+            this.query02ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query02ToolStripMenuItem1.Text = "Query 02";
+            this.query02ToolStripMenuItem1.Click += new System.EventHandler(this.query02ToolStripMenuItem1_Click);
+            // 
+            // query03ToolStripMenuItem1
+            // 
+            this.query03ToolStripMenuItem1.Name = "query03ToolStripMenuItem1";
+            this.query03ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query03ToolStripMenuItem1.Text = "Query 03";
+            this.query03ToolStripMenuItem1.Click += new System.EventHandler(this.query03ToolStripMenuItem1_Click);
+            // 
+            // query04ToolStripMenuItem1
+            // 
+            this.query04ToolStripMenuItem1.Name = "query04ToolStripMenuItem1";
+            this.query04ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query04ToolStripMenuItem1.Text = "Query 04";
+            this.query04ToolStripMenuItem1.Click += new System.EventHandler(this.query04ToolStripMenuItem1_Click);
+            // 
+            // query05ToolStripMenuItem1
+            // 
+            this.query05ToolStripMenuItem1.Name = "query05ToolStripMenuItem1";
+            this.query05ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query05ToolStripMenuItem1.Text = "Query 05";
+            this.query05ToolStripMenuItem1.Click += new System.EventHandler(this.query05ToolStripMenuItem1_Click);
+            // 
+            // query06ToolStripMenuItem1
+            // 
+            this.query06ToolStripMenuItem1.Name = "query06ToolStripMenuItem1";
+            this.query06ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query06ToolStripMenuItem1.Text = "Query 06";
+            this.query06ToolStripMenuItem1.Click += new System.EventHandler(this.query06ToolStripMenuItem1_Click);
+            // 
+            // query07ToolStripMenuItem1
+            // 
+            this.query07ToolStripMenuItem1.Name = "query07ToolStripMenuItem1";
+            this.query07ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.query07ToolStripMenuItem1.Text = "Query 07";
+            this.query07ToolStripMenuItem1.Click += new System.EventHandler(this.query07ToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridView);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "LINQ to SQL";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercise02;
+        private System.Windows.Forms.ToolStripMenuItem query01ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query02ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query03ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query04ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query05ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query06ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query07ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadDatabaseToolStripMenuItem;
+        private System.Windows.Forms.DataGridView gridView;
+        private System.Windows.Forms.ToolStripMenuItem exercise03ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem query01ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem query02ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem query03ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem query04ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem query05ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem query06ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem query07ToolStripMenuItem1;
+    }
+}
+
---
+++ Week10/Exercise02/Exercise02/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise02
+{
+    public partial class Form1 : Form
+    {
+        NorthwindSQLClassesDataContext dataContext;
+        public Form1()
+        {
+            InitializeComponent();
+            dataContext = new NorthwindSQLClassesDataContext();
+        }
+
+        private void loadDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void query01ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = dataContext.Customers.ToList();
+        }
+
+        private void query02ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = (
+                from customer in dataContext.Customers
+                where (customer.CustomerID).Trim().ToString().StartsWith("A")
+                select customer
+                );
+        }
+
+        private void query03ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var query = from customer in dataContext.Customers
+                        where customer.City == "London"
+                        select customer;
+            gridView.DataSource = query.ToList();
+            MessageBox.Show(query.Count().ToString(), "Quantity of customer in London", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void query04ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = (
+                from customer in dataContext.Customers
+                where customer.City != null
+                select new
+                {
+                    City = customer.City,
+                }
+                ).Distinct();
+        }
+
+        private void query05ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var minContactName = (from customer in dataContext.Customers
+                                 select customer.ContactName.Length).Min();
+            var maxContactName = (from customer in dataContext.Customers
+                                  select customer.ContactName.Length
+                                  ).Max();
+            var query = from customer in dataContext.Customers
+                        where customer.ContactName.Length == minContactName || customer.ContactName.Length == maxContactName
+                        select customer;
+            gridView.DataSource = query.ToList();
+        }
+
+        private void query06ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var query = from customer in dataContext.Customers
+                        group customer by customer.Country into customerGroup
+                        select new
+                        {
+                            Country = customerGroup.Key,
+                            Total = (from cust in customerGroup
+                                     select cust.Country).Count()
+                        };
+            gridView.DataSource = query.ToList();
+        }
+
+        private void query07ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var query = from customer in dataContext.Customers
+                        group customer by customer.Country into customerGroup
+                        select new
+                        {
+                            Country = customerGroup.Key,
+                            NumberOfCountry = customerGroup.Count()
+                        };
+            var query1 = from q in query
+                         where q.NumberOfCountry == (from q1 in query
+                                                     select q1.NumberOfCountry).Max()
+                         select q;
+            gridView.DataSource = query1;
+        }
+
+        // ------------ Exercises 03 ------------ //
+        private void query01ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = dataContext.Orders.ToList();
+        }
+
+        private void query02ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = (from order in dataContext.Orders
+                                   where order.CustomerID == "HANAR"
+                                   orderby order.OrderDate ascending
+                                   select order).ToList<Order>();
+        }
+
+        private void query03ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = (from order in dataContext.Orders
+                                   where order.CustomerID == "QUICK"
+                                   select new
+                                   {
+                                       CustomerID = order.CustomerID,
+                                       OrderID = order.OrderID,
+                                       ShippedDate = order.ShippedDate,
+                                       CompanyName = order.Shipper.CompanyName
+                                   }).ToList();
+        }
+
+        private void query04ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = (from order in dataContext.Orders
+                                   join orderDetail in dataContext.Order_Details
+                                   on order.OrderID equals orderDetail.OrderID
+                                   where order.OrderID.ToString() == "10259"
+                                   select new
+                                   {
+                                       OrderID = order.OrderID,
+                                       CustomerID = order.CustomerID,
+                                       ProductName = orderDetail.Product.ProductName,
+                                       UnitPrice = orderDetail.UnitPrice,
+                                       Quantity = orderDetail.Quantity,
+                                       Discount = orderDetail.Discount,
+                                       Total = (float)(orderDetail.UnitPrice) * (float)orderDetail.Quantity * (float)(1f - orderDetail.Discount)
+                                   }).ToList();
+        }
+
+        private void query05ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var query = from order in dataContext.Orders
+                        join orderDetail in dataContext.Order_Details
+                        on order.OrderID equals orderDetail.OrderID
+                        where order.OrderID.ToString() == "10259"
+                        select new
+                        {
+                            OrderID = order.OrderID,
+                            CustomerID = order.CustomerID,
+                            ProductName = orderDetail.Product.ProductName,
+                            UnitPrice = orderDetail.UnitPrice,
+                            Quantity = orderDetail.Quantity,
+                            Discount = orderDetail.Discount,
+                            Total = ((float)orderDetail.UnitPrice * (float)orderDetail.Quantity * (float)(1f - orderDetail.Discount))
+                        };
+            gridView.DataSource = query.ToList();
+            MessageBox.Show(query.Sum((i) => i.Total).ToString());
+        }
+
+        private void query06ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var query = from order in dataContext.Orders
+                        group order by order.OrderID into orderGroup
+                        select new
+                        {
+                            OrderID = orderGroup.Key,
+                            Total = (from o in orderGroup
+                                     join d in dataContext.Order_Details on o.OrderID equals d.OrderID
+                                     select (float)d.UnitPrice*(float)d.Quantity*(float)(1f-d.Discount)).Sum()
+                        };
+            gridView.DataSource = query.ToList();
+        }
+
+        private void query07ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            gridView.DataSource = dataContext.Orders.Take(5);
+        }
+    }
+}
